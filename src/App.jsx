b7e1a92b989f928261994647f9db04bbc094e29e
AsthaKirar <asthap602@gmail.com>
2024-09-28 19:54:23 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+<nav className='w-100% bg-pink-500 p-2  text-white flex justify-between'>
+  <h1>helooo</h1>
+  <h1>helooo</h1>
+  <h1>helooo</h1> <h1>helooo</h1>
+<h1>helooo</h1>
+
+
+</nav>
+    </>
+  )
+}
+
+export default App
